28c16322b344ca36e9f42800a56cc5fc82e0357a
Bartłomiej Kacperski <kontakt.hayq@gmail.com>
2025-04-19 19:08:37 +0200

    backend: added endpoints for ProjectMembers

--- ProjectManager/Controllers/ProjectMembersController.cs
+++ ProjectManager/Controllers/ProjectMembersController.cs
@@ -1,8 +1,8 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManager.Features.ProjectMembers.Commands;
 using ProjectManager.Features.ProjectMembers.Queries;
-using Microsoft.AspNetCore.Authorization;
 
 namespace ProjectManager.Controllers;
 
@@ -25,10 +25,43 @@ public class ProjectMembersController : ControllerBase
         return CreatedAtAction(nameof(GetById), new { id = member.Id }, member);
     }
 
+    [HttpPost("add-to-project")]
+    public async Task<IActionResult> AddToProject([FromBody] AddProjectMemberToProjectCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return result ? Ok() : BadRequest("Failed to add member to project.");
+    }
+
+    [HttpDelete("remove-from-project")]
+    public async Task<IActionResult> RemoveFromProject([FromBody] RemoveProjectMemberFromProjectCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return result ? Ok() : BadRequest("Failed to remove member from project.");
+    }
+
+    [HttpPut("change-role")]
+    public async Task<IActionResult> ChangeRole([FromBody] ChangeProjectMemberRoleCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        if (result is null)
+            return BadRequest("Failed to change role.");
+
+        return Ok(result);
+    }
+
+
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetById(Guid id)
     {
         var member = await _mediator.Send(new GetProjectMemberByIdQuery(id));
         return member is null ? NotFound() : Ok(member);
+    }
+
+    [HttpGet("project/{projectId:guid}")]
+    public async Task<IActionResult> GetByProjectId(Guid projectId)
+    {
+        var members = await _mediator.Send(new GetProjectMembersQuery(projectId));
+        return Ok(members);
     }
 }
