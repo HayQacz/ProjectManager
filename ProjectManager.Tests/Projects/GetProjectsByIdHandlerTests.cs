b55359c288e07b84e41b837ac9239b94f26893c7
Bartłomiej Kacperski <kontakt.hayq@gmail.com>
2025-04-19 16:53:44 +0200

    tests: added test to verify creating ProjectMembers and Users.

---
+++ ProjectManager.Tests/Projects/GetProjectsByIdHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using Microsoft.EntityFrameworkCore;
+using ProjectManager.Persistence;
+using ProjectManager.Features.Projects.Queries;
+using ProjectManager.Entities;
+
+namespace ProjectManager.Tests.Projects;
+
+public class GetProjectByIdHandlerTests
+{
+    private readonly AppDbContext _db;
+
+    public GetProjectByIdHandlerTests()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        _db = new AppDbContext(options);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnProject_WhenExists()
+    {
+        var project = new Project("From Test", "Desc");
+        _db.Projects.Add(project);
+        await _db.SaveChangesAsync();
+
+        var handler = new GetProjectByIdHandler(_db);
+        var result = await handler.Handle(new GetProjectByIdQuery(project.Id), CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.Equal(project.Id, result!.Id);
+    }
+}
